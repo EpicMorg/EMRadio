1b38ecaf52e5377f1a3bef02a29b218cac7d4504
kasthack <kasthack@epicm.org>
2013-06-29 00:19:59 +0400

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMRADIO
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool playing = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            button1.Content = playing ? "Play" : "Stop";
+            if (!playing)
+            {
+                try
+                {
+                    mediaElement1.Source = new Uri(cmb_url.Text);
+                    mediaElement1.Play();
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Error occured:\r\n" + ex.Message, "Can't start playing", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                mediaElement1.Stop();
+            }
+            playing = !playing;
+        }
+    }
+}
